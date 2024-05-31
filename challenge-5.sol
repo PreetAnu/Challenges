584cd2f8cea6775e5884c9edb9de1a0599f97ec8
PreetAnu <158500493+PreetAnu@users.noreply.github.com>
2024-05-31 14:53:43 +0530

    Created challenge-5.sol

---
+++ challenge-5.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract operation{
+    function add(uint x, uint y) external pure returns (uint){
+        return x + y;
+    }
+    function subtract(uint x, uint y) external pure returns (uint){
+        return x - y;
+    }
+    function multiply(uint x , uint y) external pure returns(uint){
+        return x * y;
+    }
+    function divide(uint x , uint y) external pure returns(uint){
+        return x / y;
+    }
+}
